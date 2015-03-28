4bc167f38719aa5ae9274ad31096b74d1dc00441
Calvyn82 <claytonflesher@gmail.com>
2015-03-28 13:28:21 -0500

    madlibs.rb

--- madlibs/madlibs.rb
+++ madlibs/madlibs.rb
@@ -2,34 +2,46 @@ class Questions
   def initialize(filename)
     @filename   = filename
     @text       = nil
-    @madlibs    = nil
   end
 
-  attr_reader :filename, :text, :madlibs
+  attr_reader :filename, :text
 
   def load_text
-    @text = File.read(filename).gsub("\n", " ")
+    @text = File.read(filename)
   end
 
   def load_madlibs
-    @madlibs = @text.scan(/\(\([^)]*\)\)/)
+    @text.gsub("\n", " ").scan(/\(\([^)]*\)\)/)
   end
 end
 
 class Answers
-  def initialize(questions = Questions.new("lunch.txt"))
+  def initialize(questions = Questions.new("gift.txt"))
     @questions  = questions
     @responses  = [ ]
     @text       = questions.load_text
+    @marked     = { }
   end
 
-  attr_reader :questions, :responses, :text
+  attr_reader :questions, :responses, :text, :marked
 
   def ask_for
     puts "Here come the madlibs."
     @questions.load_madlibs.each do |question|
-      puts question
-      @responses << gets.strip
+      query = question.match(/([^(].*[^)])/)
+      p query[1]
+      if query[1].include?(":")
+        save = query[1].match(/\A(.+):/)
+        puts question
+        answer = gets.strip
+        @marked[save[1]] = answer
+        @responses << answer
+      elsif @marked.has_key?(query[1])
+        @responses << @marked[query[1]]
+      else
+        puts question
+        @responses << gets.strip
+      end
     end
   end
 
