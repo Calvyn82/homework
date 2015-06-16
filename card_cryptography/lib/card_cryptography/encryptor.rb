faa476e480b5019e832924321ee8c4ceaafb7ad9
Calvyn82 <claytonflesher@gmail.com>
2015-06-15 19:10:23 -0500

    Rewrote tests to provide more coverage, and broke 'prepare' method out into multiple tested methods: 'discard_non_letters', 'all_caps_string', 'append_xtra' and 'space_insertion'. Also, refactored 'space_insertion' to use 'each_slice' instead of a 'while' loop

--- card_cryptography/lib/card_cryptography/encryptor.rb
+++ card_cryptography/lib/card_cryptography/encryptor.rb
@@ -7,28 +7,37 @@ module Cipher
     attr_reader :statement
 
     def prepare
-      @statement = statement
-        .chars
-        .keep_if { |a| a[/[A-Za-z]/] }
-        .join
-        .upcase
+      discard_non_letters
+      all_caps_string
       append_xtra
-      space_insertion_loop
+      space_insertion
       return statement
     end
 
+    def discard_non_letters
+      @statement = statement
+        .chars
+        .keep_if { |a| a[/[A-Za-z]/] }
+    end
+
+    def all_caps_string
+      @statement = statement
+        .join
+        .upcase
+    end
+
     def append_xtra
-      ((5 - statement.length) % 5).times do
-        @statement << "X"
+      if statement.length % 5 != 0
+        @statement << "X" * (5 - statement.length % 5)
       end
     end
 
-    def space_insertion_loop
-      i = 0
-      while i < (statement.length/5 - 1)
-        @statement.insert(5 + (5 * i) + i, " ")
-        i += 1
-      end
+    def space_insertion
+      separated = [ ]
+      @statement = statement.chars
+      statement.each_slice(5) { |group| separated << group + [" "] }
+      @statement = separated
+      @statement = statement.join.strip
     end
   end
 end
--- card_cryptography/spec/cipher_spec.rb
+++ card_cryptography/spec/cipher_spec.rb
@@ -1,14 +1,34 @@
 require_relative "../lib/card_cryptography/encryptor"
 
 describe Cipher::Encryptor do
-  it "discards any non-letter characters, upcase and return in groups of five letters" do
+  it "discards any non-letter characters" do
     statement = Cipher::Encryptor.new("Code in Ruby, live longer!")
-    expect(statement.prepare).to eq("CODEI NRUBY LIVEL ONGER")
+    expect(statement.discard_non_letters).to eq(["C", "o", "d", "e", "i", "n", "R", "u", "b", "y", "l", "i", "v", "e", "l", "o", "n", "g", "e", "r"])
   end
 
-  it "appends X's to string that's length is not a multiple of 5" do
+  it "creates an all-caps string" do
+    statement = Cipher::Encryptor.new("Code in Ruby, live longer!")
+    statement.discard_non_letters
+    expect(statement.all_caps_string).to eq("CODEINRUBYLIVELONGER")
+  end
+
+  it "appends any needed X's" do
+    statement = Cipher::Encryptor.new("Code in Ruby, be happy")
+    statement.discard_non_letters
+    statement.all_caps_string
+    expect(statement.append_xtra).to eq("CODEINRUBYBEHAPPYXXX")
+  end
+
+  it "inserts a space between every fifth letter" do
+    statement = Cipher::Encryptor.new("Code in Ruby, live longer!")
+    statement.discard_non_letters
+    statement.all_caps_string
+    statement.append_xtra
+    expect(statement.space_insertion).to eq ("CODEI NRUBY LIVEL ONGER")
+  end
+
+  it "prepares the statement to be encrypted" do
     statement = Cipher::Encryptor.new("Code in Ruby, be happy")
     expect(statement.prepare).to eq("CODEI NRUBY BEHAP PYXXX")
   end
-
 end
