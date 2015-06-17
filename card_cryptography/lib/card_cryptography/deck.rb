5da2b06e55074dbe85d8b5f196c7cbbe94c726cb
Calvyn82 <claytonflesher@gmail.com>
2015-06-17 16:01:27 -0500

    Deck now does triple cut

--- card_cryptography/lib/card_cryptography/deck.rb
+++ card_cryptography/lib/card_cryptography/deck.rb
@@ -40,6 +40,16 @@ module Cipher
       return cards
     end
 
+    def triple_cut
+      move_b_joker
+      first_group = build_first_group
+      last_group  = build_last_group
+      @cards.delete_if { |card| first_group.include?(card) || last_group.include?(card) }
+      @cards.unshift(last_group)
+      @cards << first_group
+      @cards.flatten!
+    end
+
     private
 
     def b_at_end
@@ -50,6 +60,28 @@ module Cipher
         @cards.delete("B")
         @cards.insert(1, "B")
       end
+    end
+
+    def build_first_group
+      if cards.index("A") < cards.index("B")
+        first_group = cards[0..cards.index("A")]
+        first_group.delete("A")
+      else
+        first_group = cards[0..cards.index("B")]
+        first_group.delete("B")
+      end
+      first_group
+    end
+
+    def build_last_group
+      if cards.index("A") > cards.index("B")
+        last_group = cards[cards.index("A")..-1]
+        last_group.delete("A")
+      else
+        last_group = cards[cards.index("B")..-1]
+        last_group.delete("B")
+      end
+      last_group
     end
   end
 end
